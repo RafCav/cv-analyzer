c8d7ced20125d1c2d00a0e05a57f889f140180e7
Rafael <rafael.cavallari@hotmail.com>
2025-03-15 12:22:23 -0300

    commit

---
+++ front.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+# Título do aplicativo
+st.title("Analise de Currículos")
+
+# Exibir um texto na tela
+st.write("Faça o upload de seus currículos em DOCX ou PDF")
+
+# Botão de upload de arquivo
+uploaded_file = st.file_uploader("Escolha um arquivo .zip", type=["zip"])
+
+# Verificar se um arquivo foi carregado
+if uploaded_file is not None:
+    st.success(f"Arquivo '{uploaded_file.name}' carregado com sucesso!")
+
+    # Criar um botão para fazer o download do arquivo carregado
+    st.write("Clique no botão abaixo para baixar o arquivo processado:")
+    st.download_button(
+        label="Baixar Arquivo",
+        data=uploaded_file.getvalue(),
+        file_name=uploaded_file.name,
+        mime="application/zip"
+    )
